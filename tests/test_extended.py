0a04ecf95e4e9660b7bf21e7b3b51522b868f072
Clément Chauvet <chauvetadm@gmail.com>
2023-08-21 14:24:28 +0200

    fix: simplify regex checking

--- tests/test_extended.py
+++ tests/test_extended.py
@@ -70,11 +70,11 @@ print(searchquery(documents[4]))
 document = "Gandalf is a fictional characters in Tolkien's The Lord of the Rings"
 q1 = SearchQuery('"are fictionals characters"', exact_match = False, lemma_match= True, stop_words = True)
 q2 = SearchQuery('("lord ring")', exact_match = False, lemma_match= True, stop_words = True)
-q3 = SearchQuery('"gan*lf fictional" OR "lord rings"',exact_match = False, lemma_match= True, stop_words = True )
+q3 = SearchQuery('"gan*lf * tolkien" OR "lord rings"',exact_match = False, lemma_match= True, stop_words = True )
 # call to see if the text matches the query:
 print(q1(document))
 # >>> [<eldar_extended.Match object; span=(13, 33), match = 'fictional character'>]
 print(q2(document))
 # >>> [<eldar_extended.Match object; span=(51, 68), match = 'lord ring'>]
 print(q3(document))
-# >>> [<eldar_extended.Match object; span=(0, 22), match = 'gandalf fictional'>, <eldar_extended.Match object; span=(51, 68), match = 'lord ring'>]
+# >>> [<eldar_extended.Match object; span=(0, 44), match = 'gandalf fictional character tolkien'>, <eldar_extended.Match object; span=(51, 68), match = 'lord ring'>]
